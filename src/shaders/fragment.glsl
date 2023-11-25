33a1114ea389e87b85612f438160e7cf97470dec
Glen Lee <glenleecareer@gmail.com>
2023-11-25 00:53:41 +0000

    add some uniforms

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,9 +1,11 @@
 varying vec2 vUv;
 varying vec3 vPosition;
 uniform float uTime;
+uniform vec2 uResolution;
 uniform sampler2D uTexture;
 float PI = 3.141592653589793238;
 
 void main() {
+    vec 
     gl_FragColor = vec4( vUv,0., 1.0 );
 }
